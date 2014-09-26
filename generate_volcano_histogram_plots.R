ce74dca78b38f1caa563116b262dd9c9f530f8ce
sbs87 <steve@stevenbsmith.net>
2014-09-26 12:36:52 -0400

    plotted p values less than 0.99 for better historgrams of smaller p values

--- generate_volcano_histogram_plots.R
+++ generate_volcano_histogram_plots.R
@@ -1,12 +1,14 @@
 library(ggplot2)
 rm(list=ls())
 #setwd("/Users/stevensmith/IGS/sshfs_diag/cloud/stsmith/HostReponse_LacticAcid/results/cuffdiff_out/")
-setwd("/Users/stevensmith/IGS/sshfs_medusa/")
+#setwd("/Users/stevensmith/IGS/sshfs_medusa/")
 #setwd("/Users/stevensmith/IGS/")
+setwd("/Users/stsmith/Projects/Host_Response_Lactic_Acid/diag_fuse/cloud/stsmith/HostReponse_LacticAcid/results/cuffdiff_out/")
 
 #list.files()
 #figure_location<-"/Users/stevensmith/IGS/sshfs_diag/cloud/stsmith/HostReponse_LacticAcid/results/stats_and_summaries/"
-figure_location<-"/Users/stevensmith/IGS/temp/"
+#figure_location<-"/Users/stevensmith/IGS/temp/"
+figure_location<-"/Users/stsmith/Projects/Host_Response_Lactic_Acid/diag_fuse/cloud/stsmith/HostReponse_LacticAcid/results/stats_and_summaries/"
 
 (comparisons<-list.files()[grep("_vs_",list.files())])
 (comparisons<-comparisons[grep("txt",comparisons,invert=T)])
@@ -24,19 +26,19 @@ for(comparison_name in comparisons){
   #diff.exp[abs(diff.exp$log2.fold_change.)>1.7976e+308,]
   diff.exp$log2.fold_change.[abs(diff.exp$log2.fold_change.)>1.7976e+308]<-NA
   #head(diff.exp)
-  diff.exp.filter<-diff.exp[diff.exp$q_value<1,]
+  diff.exp.filter<-diff.exp[diff.exp$q_value<.99,]
   #hist(diff.exp.filter$q_value)
   #data.frame(diff.exp$log2.fold_change.,diff.exp$q_value)
 
-  postscript(file=paste(figure_location,comparison_name,".Philippe.pvalue_histogram.ps",sep=""))
+  postscript(file=paste(figure_location,comparison_name,".Pvalue_lt_0.99.pvalue_histogram.ps",sep=""))
   histogram<-ggplot(diff.exp.filter)+geom_histogram(aes(fill=significant,x=q_value))+theme_bw()+ggtitle(file_name)
   plot(histogram)
   dev.off()
 
-  postscript(file=paste(figure_location,comparison_name,".Philippe.volcano.ps",sep=""))
-  volcano<-ggplot(diff.exp)+geom_point(aes(x=log2.fold_change.,y=-log(diff.exp$q_value,base=10)))+
-    theme_bw()+ggtitle(paste("Volcano Plot",file_name))+xlab("Log2 Fold Change")+ylab("-log10(Adj p-value")+
-    geom_hline(yintercept=-log(.05,base=10),color='red')+geom_vline(xintercept=c(-1.5,1.5),color='red')
-  plot(volcano)
-  dev.off()
+  #postscript(file=paste(figure_location,comparison_name,".volcano.ps",sep=""))
+  #volcano<-ggplot(diff.exp)+geom_point(aes(x=log2.fold_change.,y=-log(diff.exp$q_value,base=10)))+
+  #  theme_bw()+ggtitle(paste("Volcano Plot",file_name))+xlab("Log2 Fold Change")+ylab("-log10(Adj p-value")+
+  #  geom_hline(yintercept=-log(.05,base=10),color='red')+geom_vline(xintercept=c(-1.5,1.5),color='red')
+  #plot(volcano)
+  #dev.off()
 }
